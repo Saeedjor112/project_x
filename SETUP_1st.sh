d51a2b78b1372f92284d60043e7f629d87731917
Saeedjor112 <64524518+Saeedjor112@users.noreply.github.com>
2020-10-16 03:27:14 +0300

    Add files via upload
    Information about the project: The project is a process of cheating the game for PUBG Mobile, which has root on all versions, international, Korean, Vietnamese or Taiwanese. I hope to present you a distinguished project

---
+++ SETUP_1st.sh
@@ -0,0 +1,24 @@
+termux-setup-storage
+pkg upgrade && pkg update
+pkg install openssl
+pkg install neofetch
+pkg install tsu
+pkg install dialog
+pkg install python
+pip install lolcat
+pkg install pv
+pkg install screenfetch
+pkg install toilet
+pkg install figlet 
+pkg install sox
+pkg install cowsay
+pkg install termux-api
+pkg install cmatrix
+pkg install openssl-tool
+pkg install tor
+pkg install python -y 
+pkg install pv -y 
+pip install lolcat
+pkg install wget -y
+pkg install nodejs -y
+npm install --global internal-ip-cli
